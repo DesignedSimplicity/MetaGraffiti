278d32dc037934d504150403c8d3950c7fb06722
Kevin <Kevin@X>
2018-03-07 13:48:28 +1300

    Updated and consolidated all references and dependencies

---
+++ MetaGraffiti.Base/Modules/Xlsx/XlsxFileReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetaGraffiti.Base.Modules.Xlsx
+{
+    public class XlsxFileReader
+    {
+		//private OleDbConnection
+		//Provider=Microsoft.ACE.OLEDB.12.0;Data Source=c:\myFolder\myExcel2007file.xlsx;Extended Properties = "Excel 12.0 Xml;HDR=YES";
+
+		// ==================================================
+		// Constructors
+		public XlsxFileReader(string uri)
+		{
+
+		}
+	}
+}
