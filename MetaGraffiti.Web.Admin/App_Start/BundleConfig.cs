bca58ebc5540fed21899d38952a3eb5e2fdbf883
X2 <office@designed.com>
2020-01-18 21:07:52 +1300

    Upgraded packages and versions

--- MetaGraffiti.Web.Admin/App_Start/BundleConfig.cs
+++ MetaGraffiti.Web.Admin/App_Start/BundleConfig.cs
@@ -20,7 +20,7 @@ namespace MetaGraffiti.Web.Admin
 
 			bundles.Add(new StyleBundle("~/bundle/css")
 						.Include("~/Content/bootstrap.css")
-						.Include("~/Content/bootstrap-responsive.css")
+						//.Include("~/Content/bootstrap-responsive.css")
 						.Include("~/Content/font-awesome.css")
 						.Include("~/Content/flags.css")
 						.Include("~/Content/_graffiti/common.css")
